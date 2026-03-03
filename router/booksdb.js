71183cb3f47065e166e076f893fc3082f3c68e3b
YahyaEajass05 <yahyaeajass05@gmail.com>
2026-03-03 13:33:40 +0530

    fix: update author names and fix encoding issue for book 9

--- router/booksdb.js
+++ router/booksdb.js
@@ -1,11 +1,11 @@
 let books = {
   1: { isbn: '978-0-435-90227-9', author: 'Chinua Achebe', title: 'Things Fall Apart', reviews: {} },
-  2: { isbn: '978-0-14-303943-3', author: 'Hans Christian Andersen', title: 'Fairy tales', reviews: {} },
+  2: { isbn: '978-0-14-303943-3', author: 'Hans Christian Andersen', title: 'Fairy Tales', reviews: {} },
   3: { isbn: '978-0-14-243723-6', author: 'Dante Alighieri', title: 'The Divine Comedy', reviews: {} },
-  4: { isbn: '978-0-14-044919-8', author: 'Unknown', title: 'The Epic Of Gilgamesh', reviews: {} },
-  5: { isbn: '978-0-19-953559-9', author: 'Unknown', title: 'The Book Of Job', reviews: {} },
-  6: { isbn: '978-0-14-044289-2', author: 'Unknown', title: 'One Thousand and One Nights', reviews: {} },
-  7: { isbn: '978-0-14-044769-9', author: 'Unknown', title: 'Njaals Saga', reviews: {} },
+  4: { isbn: '978-0-14-044919-8', author: 'Sin-liqe-unninni', title: 'The Epic Of Gilgamesh', reviews: {} },
+  5: { isbn: '978-0-19-953559-9', author: 'Anonymous', title: 'The Book Of Job', reviews: {} },
+  6: { isbn: '978-0-14-044289-2', author: 'Anonymous', title: 'One Thousand and One Nights', reviews: {} },
+  7: { isbn: '978-0-14-044769-9', author: 'Anonymous', title: 'Njaals Saga', reviews: {} },
   8: { isbn: '978-0-14-143951-8', author: 'Jane Austen', title: 'Pride and Prejudice', reviews: {} },
   9: { isbn: '978-0-14-044319-6', author: 'Honore de Balzac', title: 'Le Pere Goriot', reviews: {} },
   10: { isbn: '978-0-8021-4475-9', author: 'Samuel Beckett', title: 'Molloy, Malone Dies, The Unnamable, the trilogy', reviews: {} }
